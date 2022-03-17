2ee9474ba46a2f3173b3f49784ade9f53f26cb25
Felipe Rocha <felipemota.rocha@gmail.com>
2022-03-17 16:14:06 -0300

    feat: show CategoryItem for each Category

--- src/components/categories/categories.component.tsx
+++ src/components/categories/categories.component.tsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
-// Utilities
-import Category from '../../types/category.types'
-import env from '../../config/env.config'
+// Components
+import CategoryItem from '../category-item/category-item.component'
 
 // Styles
 import './categories.styles.css'
+
+// Utilities
+import Category from '../../types/category.types'
+import env from '../../config/env.config'
 
 const Categories = () => {
   const [categories, setCategories] = useState<Category[]>([])
@@ -30,7 +33,11 @@ const Categories = () => {
   return (
     <div className="categories-container">
       <div className="categories-content">
-        {/* {categories.map(category => <CategoryItem)} */}
+        {categories.map((category) => (
+          <div key={category.id}>
+            <CategoryItem category={category} />
+          </div>
+        ))}
       </div>
     </div>
   )
