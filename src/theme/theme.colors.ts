dd8845b6d2b0027ef54f5252cc3b12a513e9f606
Felipe Rocha <felipemota.rocha@gmail.com>
2022-03-17 16:41:10 -0300

    feat: add Theme Colors file

---
+++ src/theme/theme.colors.ts
@@ -0,0 +1,18 @@
+const Colors = {
+  primary: '#212529',
+  text: {
+    white: '#F8F9FA',
+    dark: '#343A40'
+  },
+  background: {
+    dark: '#212529',
+    white: '#F8F9FA'
+  },
+  input: {
+    background: '#E9ECEF',
+    placeholder: '#6C757D'
+  },
+  error: '#FF6A6A'
+}
+
+export default Colors
