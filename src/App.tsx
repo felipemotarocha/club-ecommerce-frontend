0f17799648becfd3d38eda1f1f6400863040fec6
Felipe Rocha <felipemota.rocha@gmail.com>
2022-03-17 15:44:08 -0300

    feat: add Home Page and React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { FunctionComponent } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-// Components
-import Header from './components/header/header.component'
+// Pages
+import HomePage from './pages/home/home.page'
 
-interface AppProps {
-  message?: string
-}
-
-const App: FunctionComponent<AppProps> = ({ message }) => {
-  return <Header />
+const App: FunctionComponent = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
---
+++ src/pages/home/home.page.tsx
@@ -0,0 +1,12 @@
+// Components
+import Header from '../../components/header/header.component'
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default HomePage
