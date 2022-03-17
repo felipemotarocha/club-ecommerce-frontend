3956b0a8187c615f2a09891207712457b05b56cd
Felipe Rocha <felipemota.rocha@gmail.com>
2022-03-17 16:13:45 -0300

    fix: update Category Item background image

--- src/components/category-item/category-item.component.tsx
+++ src/components/category-item/category-item.component.tsx
@@ -14,7 +14,7 @@ const CategoryItem: FunctionComponent<CategoryItemProps> = ({ category }) => {
   return (
     <div
       className="category-item-container"
-      style={{ backgroundImage: category.imageUrl }}>
+      style={{ backgroundImage: `url('${category.imageUrl}')` }}>
       <div className="category-name">
         <p>{category.displayName}</p>
         <p>Explorar</p>
