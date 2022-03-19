2a1607b3c70161ef6814120a622461f52ca13b5b
Felipe Rocha <felipemota.rocha@gmail.com>
2022-03-19 14:37:42 -0300

    feat: add Custom Button Component

---
+++ src/components/custom-button/custom-button.component.tsx
@@ -0,0 +1,23 @@
+import React, { FunctionComponent, ButtonHTMLAttributes } from 'react'
+
+import { CustomButtonContainer, IconContainer } from './custom-button.styles'
+
+interface CustomButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  startIcon?: React.ReactNode
+}
+
+const CustomButton: FunctionComponent<CustomButtonProps> = ({
+  children,
+  startIcon,
+  ...rest
+}) => {
+  return (
+    <CustomButtonContainer {...rest}>
+      {startIcon && <IconContainer>{startIcon}</IconContainer>}
+
+      {children}
+    </CustomButtonContainer>
+  )
+}
+
+export default CustomButton
