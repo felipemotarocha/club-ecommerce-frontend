a7f73a03cc354add64307cd5111c807faa025564
Felipe Rocha <felipemota.rocha@gmail.com>
2022-06-15 08:43:09 -0300

    feat: add Product type

--- src/types/category.types.ts
+++ src/types/category.types.ts
@@ -1,8 +1,11 @@
+import Product from './product.types'
+
 interface Category {
   id: string
   name: string
   displayName: string
   imageUrl: string
+  products: Product[]
 }
 
 export default Category
