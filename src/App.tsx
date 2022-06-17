1b09c4cd910aacf339b1df7d8be258b09cbe4192
Felipe Rocha <felipemota.rocha@gmail.com>
2022-06-17 19:20:06 -0300

    feat: add auth state listener and sign out button

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { onAuthStateChanged } from 'firebase/auth'
 import { FunctionComponent } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
@@ -6,7 +7,14 @@ import HomePage from './pages/home/home.page'
 import LoginPage from './pages/login/login.page'
 import SignUpPage from './pages/sign-up/sign-up.page'
 
+// Utilities
+import { auth } from './config/firebase.config'
+
 const App: FunctionComponent = () => {
+  onAuthStateChanged(auth, (user) => {
+    console.log(user)
+  })
+
   return (
     <BrowserRouter>
       <Routes>
--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,5 +1,7 @@
+import { signOut } from 'firebase/auth'
 import { BsCart3 } from 'react-icons/bs'
 import { useNavigate } from 'react-router-dom'
+import { auth } from '../../config/firebase.config'
 
 import {
   HeaderContainer,
@@ -27,6 +29,7 @@ const Header = () => {
         <HeaderItem>Explorar</HeaderItem>
         <HeaderItem onClick={handleLoginClick}>Login</HeaderItem>
         <HeaderItem onClick={handleSignUpClick}>Criar Conta</HeaderItem>
+        <HeaderItem onClick={() => signOut(auth)}>Sair</HeaderItem>
         <HeaderItem>
           <BsCart3 size={25} />
           <p style={{ marginLeft: 5 }}>5</p>
