3db8e254d8f72b464bc966f3585c02b24a2d5f85
Felipe Rocha <felipemota.rocha@gmail.com>
2022-07-11 18:32:57 -0300

    feat: add initial redux setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
@@ -7,16 +8,19 @@ import reportWebVitals from './reportWebVitals'
 import UserContextProvider from './contexts/user.context'
 import CategoryContextProvider from './contexts/category.context'
 import CartContextProvider from './contexts/cart.context'
+import store from './store/store'
 
 ReactDOM.render(
   <React.StrictMode>
-    <UserContextProvider>
-      <CategoryContextProvider>
-        <CartContextProvider>
-          <App />
-        </CartContextProvider>
-      </CategoryContextProvider>
-    </UserContextProvider>
+    <Provider store={store}>
+      <UserContextProvider>
+        <CategoryContextProvider>
+          <CartContextProvider>
+            <App />
+          </CartContextProvider>
+        </CategoryContextProvider>
+      </UserContextProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
