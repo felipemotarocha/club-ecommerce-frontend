62a17b7fce525b7961307c316f1d3de07c99f551
Felipe Rocha <felipemota.rocha@gmail.com>
2022-08-02 11:07:23 -0300

    style: add responsiveness to Header component

--- src/components/header/header.styles.ts
+++ src/components/header/header.styles.ts
@@ -8,6 +8,12 @@ export const HeaderContainer = styled.div`
   justify-content: space-between;
   padding: 20px;
   color: ${Colors.text.white};
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
 `
 
 export const HeaderTitle = styled.h2`
@@ -17,12 +23,21 @@ export const HeaderTitle = styled.h2`
   &:hover {
     cursor: pointer;
   }
+
+  @media (max-width: 768px) {
+    margin-bottom: 16px;
+  }
 `
 
 export const HeaderItems = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+
+  @media (max-width: 768px) {
+    width: 100%;
+    justify-content: space-evenly;
+  }
 `
 
 export const HeaderItem = styled.div`
@@ -35,6 +50,10 @@ export const HeaderItem = styled.div`
   &:nth-child(2),
   &:nth-child(3) {
     margin-right: 40px;
+
+    @media (max-width: 768px) {
+      margin-right: 0;
+    }
   }
 
   &:hover {
