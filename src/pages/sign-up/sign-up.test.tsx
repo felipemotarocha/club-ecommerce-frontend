d4a15c7cb30c471dfb8f1cc7e3c560bd010d56b9
Felipe Rocha <felipemota.rocha@gmail.com>
2022-09-10 20:15:55 -0300

    test: ensure sign up shows error if email already exists

--- src/pages/sign-up/sign-up.test.tsx
+++ src/pages/sign-up/sign-up.test.tsx
@@ -1,6 +1,11 @@
 import userEvent from '@testing-library/user-event'
+import * as firebaseAuth from 'firebase/auth'
+import { AuthErrorCodes } from 'firebase/auth'
+
 import { renderWithRedux } from '../../helpers/test.helpers'
 import SignUpPage from './sign-up.page'
+
+jest.mock('firebase/auth')
 
 describe('Sign Up', () => {
   it('should show error when trying to submit without filling all required fields', async () => {
@@ -70,5 +75,38 @@ describe('Sign Up', () => {
     userEvent.click(submitButton)
 
     await findByText(/a senha precisa ter no mínimo 6 caracteres./i)
+  })
+
+  it('should show error if email already exists', async () => {
+    const mockFirebaseAuth = firebaseAuth as any
+
+    const { getByText, findByText, getByPlaceholderText } = renderWithRedux(
+      <SignUpPage />,
+      {}
+    )
+
+    mockFirebaseAuth.createUserWithEmailAndPassword.mockImplementation(() =>
+      Promise.reject({ code: AuthErrorCodes.EMAIL_EXISTS })
+    )
+
+    const nameInput = getByPlaceholderText(/digite seu nome/i)
+    const lastName = getByPlaceholderText(/digite seu sobrenome/i)
+    const emailInput = getByPlaceholderText(/digite seu e-mail/i)
+    const passwordInput = getByPlaceholderText(/digite sua senha/i)
+    const passwordConfirmationInput = getByPlaceholderText(
+      /digite novamente sua senha/i
+    )
+
+    userEvent.type(nameInput, 'Lorem')
+    userEvent.type(lastName, 'Ipsum')
+    userEvent.type(emailInput, 'lorem@ipsum.com')
+    userEvent.type(passwordInput, '12345678')
+    userEvent.type(passwordConfirmationInput, '12345678')
+
+    const submitButton = getByText('Criar Conta', { selector: 'button' })
+
+    userEvent.click(submitButton)
+
+    await findByText(/este e-mail já está sendo utilizado./i)
   })
 })
