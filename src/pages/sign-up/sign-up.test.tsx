056acf2dc982edd8f99c9f7827e6eb4a070791ef
Felipe Rocha <felipemota.rocha@gmail.com>
2022-09-10 20:10:08 -0300

    test: ensure sign up shows error if password and password confirmation are different

--- src/pages/sign-up/sign-up.test.tsx
+++ src/pages/sign-up/sign-up.test.tsx
@@ -33,4 +33,25 @@ describe('Sign Up', () => {
 
     await findByText(/por favor, insira um e-mail válido./i)
   })
+
+  it('should show error when password and password confirmation are different', async () => {
+    const { getByText, findByText, getByPlaceholderText } = renderWithRedux(
+      <SignUpPage />,
+      {}
+    )
+
+    const passwordInput = getByPlaceholderText(/digite sua senha/i)
+    const passwordConfirmationInput = getByPlaceholderText(
+      /digite novamente sua senha/i
+    )
+
+    userEvent.type(passwordInput, '123456')
+    userEvent.type(passwordConfirmationInput, '12345678')
+
+    const submitButton = getByText('Criar Conta', { selector: 'button' })
+
+    userEvent.click(submitButton)
+
+    await findByText(/a confirmação de senha precisa ser igual a senha./i)
+  })
 })
