a30bc820e1a817374888142dbd3366b4a56e2032
Felipe Rocha <felipemota.rocha@gmail.com>
2022-09-10 15:05:21 -0300

    feat: make preloadedState on renderWithRedux optional

--- src/helpers/test.helpers.tsx
+++ src/helpers/test.helpers.tsx
@@ -10,14 +10,14 @@ import { RootState } from '../store/store'
 export const renderWithRedux = (
   component: React.ReactElement,
   {
-    preloadedState,
+    preloadedState = {},
     store = configureStore({
       reducer: rootReducer,
       preloadedState
     }),
     ...renderOptions
   }: {
-    preloadedState: RootState
+    preloadedState?: Partial<RootState>
     store?: any
   }
 ) => {
